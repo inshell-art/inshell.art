c087501d8abf27905dd374070c91da25e0b1d8f1
inshell <this.inshell@gmail.com>
2024-08-03 17:40:53 +0800

    DONE firebase config

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: "dist",
-  },
-  server: {
-    host: true,
-  },
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [react()],
+    build: {
+      outDir: "dist",
+    },
+    server: {
+      host: true,
+    },
+    envDir: "./config",
+    define: {
+      "import.meta.env.MODE": JSON.stringify(mode),
+    },
+  };
 });
